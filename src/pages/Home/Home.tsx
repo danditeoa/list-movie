74b8e6a615e0ff37fb0f160fe2b2ad9caac8cd19
DandiTeoa <taci.dandi@gmail.com>
2022-10-27 21:13:01 -0300

    feat(loading): create setLoading

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,18 +9,23 @@ const Home = () => {
   const dispatch = useDispatch();
   const [data, setData] = useState()
   const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
   const fetchData = async (page: number) => {
-    // setLoading(true)
+    setLoading(true)
     const response = await fetchPopularMovies(page)
     setData(response.data)
     dispatch(addPopular(response.data))
     console.log(response)
-    // setLoading(false)
+    setLoading(false)
   }
   const popular = useSelector(getPopularMoviesSelector);
-useEffect(()=> {
-fetchData(page)
-},[page])
+  useEffect(()=> {
+    fetchData(page)
+  },[page])
+
+  if(loading){
+    return <div><h1>Loading ...</h1></div>
+  }
   return (
     <div>works
       <div>
