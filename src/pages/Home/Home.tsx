be383869dd006d208eb2a5d1ac856de369529144
DandiTeoa <taci.dandi@gmail.com>
2022-10-31 15:50:42 -0300

    feat(pagination): create pagination

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Pagination from "../../components/Pagination";
 import { addPopular } from "../../redux/popular/popular-actions";
 import { getPopularMoviesSelector } from "../../redux/popular/popular-selectors";
 import { fetchPopularMovies } from "../../services/moviesAPI";
@@ -23,18 +24,35 @@ const Home = () => {
     fetchData(page)
   },[page])
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(1);
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = popular.results.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber: React.SetStateAction<number>) => setCurrentPage(pageNumber);
+
   if(loading){
     return <div><h1>Loading ...</h1></div>
   }
   return (
     <div>works
-      <div>
+      {/* <div>
         {popular.page}
       </div>
       <div>
         {popular && popular.results.map(movie => <div>{movie.title}</div>)}
+      </div> */}
+      <div className="mt-5 mb-3">
+        <b>Current Page:</b><span className="ml-2">{currentPage}</span>
       </div>
-      
+<Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={popular.results.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
