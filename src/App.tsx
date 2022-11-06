e57e82bb6ccb7b96793a3557df7497587e2f7efc
DandiTeoa <taci.dandi@gmail.com>
2022-11-06 16:40:57 -0300

    feat: update app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Movie from "./pages/Movie/Movie";
+import store from "./redux/store";
 import './styles/index.scss'
 
 const App = () => {
 
   return (
-    <>
-     <Home/>
-    </>
+    <Provider store={store}>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie" element={<Movie />} />
+      </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
