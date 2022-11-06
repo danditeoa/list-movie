66f536bfa357a8aa4953ec2445f33c8bd07ab5a3
DandiTeoa <taci.dandi@gmail.com>
2022-11-06 16:35:39 -0300

    feat: edit pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
+import './Pagination.scss';
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }:any) => {
+const Pagination = ({ moviesPerPage, totalMovies, paginate }:any) => {
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalMovies / moviesPerPage); i++) {
     pageNumbers.push(i);
   }
 
   return (
-    <nav>
-      <ul className='pagination'>
-        {pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link'>
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <ul className="pagination-container">
+      {pageNumbers.map(page => (
+        <li key={page} className='page-item'>
+          <span onClick={() => paginate(page)} className='page'>
+            {page}
+          </span>
+        </li>
+      ))}
+    </ul>
   );
 };
 
