ea44fc6a26a7c764179331275e12c7c996c80e15
DandiTeoa <taci.dandi@gmail.com>
2022-11-06 16:34:21 -0300

    feat: create CardList component

---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Movie } from '../../models/paginatedMovie.model';
+import CardMovie from '../cardMovie/CardMovie';
+import "./CardList.scss";
+
+export type CardListProp= {
+  list: Movie[]
+} 
+const CardList = ({list}: CardListProp) => {
+  if(!list) {
+  return null;
+  }
+
+  return (
+      <div className="container">
+        {list.map(movie =><CardMovie key={movie.id} movie={movie} />)}
+      </div> 
+  );
+};
+
+export default CardList;
